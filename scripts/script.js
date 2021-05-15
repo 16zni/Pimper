26ee9bc77e7147fbaabc4c741043a9cafc5ced25
16zni <16zni.pro@gmail.com>
2021-05-15 11:35:31 +0200

    Update 15-05-2021

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 let count = 649;
 window.setInterval(function counter(){
-  if(Math.random() <= 0.3 || Math.random() > 0.7) document.querySelector(".counter2").innerHTML = ++count < 10 ? "00" + count : count < 100 && count > 9 ? "0" + count : count;
+  if(Math.random() <= 0.2 || Math.random() > 0.8) document.querySelector(".counter2").innerHTML = ++count < 10 ? "00" + count : count < 100 && count > 9 ? "0" + count : count;
   if(count >= 999){
     count = 0;
     ++document.querySelector(".counter1").innerHTML;
